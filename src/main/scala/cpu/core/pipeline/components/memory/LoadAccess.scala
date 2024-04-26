1dac88a40e3cd1f28e66738b044a410fd5cff3f6
crpboy <crpboy2004@gmail.com>
2024-04-26 18:27:04 +0800

    refactor: add memory unit LoadAccess to process load data, remove relavent code in MemoryUnit

---
+++ src/main/scala/cpu/core/pipeline/components/memory/LoadAccess.scala
@@ -0,0 +1,90 @@
+package cpu.core.pipeline.components.memory
+
+import chisel3._
+import chisel3.util._
+
+import cpu.utils.Functions._
+import cpu.common._
+import cpu.common.Const._
+
+class LoadAccess extends Module {
+  val io = IO(new Bundle {
+    val dCache  = new Bundle { val sram_rdata = Input(UInt(DATA_WIDTH.W)) }
+    val inst    = Input(new InstInfo)
+    val data    = Input(UInt(DATA_WIDTH.W))
+    val memByte = Input(UInt(2.W))
+    val out     = Output(UInt(DATA_WIDTH.W))
+  })
+  val rdata = io.dCache.sram_rdata
+  val word = Mux(
+    !io.inst.fuop(2).asBool,
+    rdata,
+    MuxLookup(
+      io.inst.fuop,
+      0.U,
+      Seq(
+        mem_lb -> MuxLookup(
+          io.memByte,
+          0.U,
+          Seq(
+            "b00".U -> signedExtend(rdata(7, 0)),
+            "b01".U -> signedExtend(rdata(15, 8)),
+            "b10".U -> signedExtend(rdata(23, 16)),
+            "b11".U -> signedExtend(rdata(31, 24)),
+          ),
+        ),
+        mem_lbu -> MuxLookup(
+          io.memByte,
+          0.U,
+          Seq(
+            "b00".U -> zeroExtend(rdata(7, 0)),
+            "b01".U -> zeroExtend(rdata(15, 8)),
+            "b10".U -> zeroExtend(rdata(23, 16)),
+            "b11".U -> zeroExtend(rdata(31, 24)),
+          ),
+        ),
+        mem_lh -> Mux(
+          io.memByte(1).asBool,
+          signedExtend(rdata(31, 16)),
+          signedExtend(rdata(15, 0)),
+        ),
+        mem_lhu -> Mux(
+          io.memByte(1).asBool,
+          zeroExtend(rdata(31, 16)),
+          zeroExtend(rdata(15, 0)),
+        ),
+      ),
+    ),
+  )
+  val data = Mux(
+    io.inst.fu === fu_mem && io.inst.wb,
+    MuxLookup(
+      io.inst.fuop,
+      word,
+      Seq(
+        mem_lwl -> MuxLookup(
+          io.memByte,
+          0.U,
+          Seq(
+            "b00".U -> Cat(word(7, 0), io.data(23, 0)),
+            "b01".U -> Cat(word(15, 0), io.data(15, 0)),
+            "b10".U -> Cat(word(23, 0), io.data(7, 0)),
+            "b11".U -> word,
+          ),
+        ),
+        mem_lwr -> MuxLookup(
+          io.memByte,
+          0.U,
+          Seq(
+            "b00".U -> word,
+            "b01".U -> Cat(io.data(31, 24), word(31, 8)),
+            "b10".U -> Cat(io.data(31, 16), word(31, 16)),
+            "b11".U -> Cat(io.data(31, 8), word(31, 24)),
+          ),
+        ),
+      ),
+    ),
+    io.data,
+  )
+  io.out := data
+}
--- src/main/scala/cpu/core/pipeline/components/memory/MemoryUnit.scala
+++ src/main/scala/cpu/core/pipeline/components/memory/MemoryUnit.scala
@@ -17,91 +17,24 @@ class MemoryUnit extends Module {
     val ctrlreq = Output(new CtrlRequest)
   })
 
-  val input   = io.in.bits
-  val output  = io.out
+  val input  = io.in.bits
+  val output = io.out
 
-  val rdata = io.dCache.sram_rdata
-  val lbres = MuxLookup(
-    input.memByte,
-    0.U,
-    Seq(
-      "b00".U -> signedExtend(rdata(7, 0)),
-      "b01".U -> signedExtend(rdata(15, 8)),
-      "b10".U -> signedExtend(rdata(23, 16)),
-      "b11".U -> signedExtend(rdata(31, 24)),
-    ),
-  )
-  val lbures = MuxLookup(
-    input.memByte,
-    0.U,
-    Seq(
-      "b00".U -> zeroExtend(rdata(7, 0)),
-      "b01".U -> zeroExtend(rdata(15, 8)),
-      "b10".U -> zeroExtend(rdata(23, 16)),
-      "b11".U -> zeroExtend(rdata(31, 24)),
-    ),
-  )
-  val word = Mux(
-    !input.inst.fuop(2).asBool,
-    rdata,
-    MuxLookup(
-      input.inst.fuop,
-      0.U,
-      Seq(
-        mem_lb  -> lbres,
-        mem_lbu -> lbures,
-        mem_lh -> Mux(
-          input.memByte(1).asBool,
-          signedExtend(rdata(31, 16)),
-          signedExtend(rdata(15, 0)),
-        ),
-        mem_lhu -> Mux(
-          input.memByte(1).asBool,
-          zeroExtend(rdata(31, 16)),
-          zeroExtend(rdata(15, 0)),
-        ),
-      ),
-    ),
-  )
-  val data = Mux(
-    input.inst.fu === fu_mem && input.inst.wb,
-    MuxLookup(
-      input.inst.fuop,
-      word,
-      Seq(
-        mem_lwl -> MuxLookup(
-          input.memByte,
-          0.U,
-          Seq(
-            "b00".U -> Cat(word(7, 0), input.data(23, 0)),
-            "b01".U -> Cat(word(15, 0), input.data(15, 0)),
-            "b10".U -> Cat(word(23, 0), input.data(7, 0)),
-            "b11".U -> word,
-          ),
-        ),
-        mem_lwr -> MuxLookup(
-          input.memByte,
-          0.U,
-          Seq(
-            "b00".U -> word,
-            "b01".U -> Cat(input.data(31, 24), word(31, 8)),
-            "b10".U -> Cat(input.data(31, 16), word(31, 16)),
-            "b11".U -> Cat(input.data(31, 8), word(31, 24)),
-          ),
-        ),
-      ),
-    ),
-    input.data,
-  )
+  val load = Module(new LoadAccess).io
+
+  load.dCache  <> io.dCache
+  load.inst    <> input.inst
+  load.data    <> input.data
+  load.memByte <> input.memByte
 
   io.dHazard.wen   := input.inst.wb
   io.dHazard.waddr := input.inst.rd
-  io.dHazard.wdata := data
+  io.dHazard.wdata := load.out
 
   io.ctrlreq.block := false.B
   io.ctrlreq.clear := false.B
 
-  output.data     := data
+  output.data     := load.out
   output.inst     := input.inst
   output.pc       := input.pc
   output.debug_pc := input.debug_pc
